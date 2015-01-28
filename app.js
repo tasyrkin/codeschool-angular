07a782dbd4cd626785d22640ea8618f7625d041f
Timofey Asyrkin <tasyrkin@gmail.com>
2015-01-28 18:44:06 +0100

    Showing multiple products on the page

---
+++ app.js
@@ -0,0 +1,24 @@
+(function() {
+  
+  var app = angular.module("store", []);
+  
+  app.controller("StoreController", function(){
+    this.products = gems; 
+  });
+
+  var gems = [
+    {
+      name: "Dodecahedron",
+      price: 2.95,
+      description: "Dodecahedron is a very good thing!",
+      canPurchase: true,
+      soldOut: true
+    },
+    {
+      name: "Dodecahedron2",
+      price: 10.0,
+      description: "Dodecahedron2 is much better thing!",
+      canPurchase: true,
+    }
+  ];
+})();
